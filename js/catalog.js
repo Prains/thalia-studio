ded45fa87788b798af63350f337452638d0a3c04
prains <sleskov2013@yandex.ru>
2022-10-30 12:35:04 +0300

    catalog mobile version plus catalog sorting

--- js/catalog.js
+++ js/catalog.js
@@ -6,75 +6,87 @@ const cards = [
     title: "Торба",
     price: 4200,
     order: 1,
+    type: 0,
   },
   {
     img: "../images/catalog2.jpg",
     title: "Сумка с бахромой",
     price: 5200,
     order: 2,
+    type: 0,
   },
   {
     img: "../images/catalog3.jpg",
     title: "Дорожка на стол",
     price: 2000,
     order: 3,
+    type: 1,
   },
   {
     img: "../images/catalog4.jpg",
     title: "Салфетница",
     price: 1500,
     order: 4,
+    type: 1,
   },
   {
     img: "../images/catalog5.jpg",
     title: "Сумка Таби",
     price: 3200,
     order: 5,
+    type: 0,
   },
   {
     img: "../images/catalog6.jpg",
     title: "Сумка летняя Сансара",
     price: 5500,
     order: 6,
+    type: 0,
   },
   {
     img: "../images/catalog7.jpg",
     title: "Сумка с рогаликами",
     price: 4200,
     order: 7,
+    type: 0,
   },
   {
     img: "../images/catalog8.jpg",
     title: "Кардхолдер",
     price: 500,
     order: 8,
+    type: 1,
   },
   {
     img: "../images/catalog9.jpg",
     title: "Бутылочница из джута",
     price: 1500,
     order: 9,
+    type: 1,
   },
   {
     img: "../images/catalog10.jpg",
     title: "Лежанка для питомцев",
     price: 3000,
     order: 10,
+    type: 1,
   },
   {
     img: "../images/catalog11.jpg",
     title: "Набор корзин «Свадебные»",
     price: 3000,
     order: 11,
+    type: 1,
   },
   {
     img: "../images/catalog12.jpg",
     title: "Корзина «Рыбка»",
     price: 1200,
     order: 12,
+    type: 1,
   },
 ];
-const tags = [];
+
 const filterPopup = document.querySelector(
   ".catalog-page__filter__search__dropdown__popup"
 );
@@ -84,6 +96,11 @@ const filterText = document.querySelector(
 const popupText = document.querySelectorAll(
   ".catalog-page__filter__search__dropdown__popup__text"
 );
+const bagsButton = document.querySelector(".catalog-page__filter__items__bags");
+const decorButton = document.querySelector(
+  ".catalog-page__filter__items__decor"
+);
+const allButton = document.querySelector(".catalog-page__filter__items__all");
 
 const filter = [
   {
@@ -207,3 +224,41 @@ for (let i = 0; i < popupText.length; i++) {
     filter[i].function();
   });
 }
+
+bagsButton.addEventListener("click", () => {
+  removeElements();
+  bagsButton.classList.add("selected");
+  decorButton.classList.remove("selected");
+  allButton.classList.remove("selected");
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].type == 0)
+      items.prepend(
+        createElement(template, cards[i].img, cards[i].title, cards[i].price)
+      );
+  }
+});
+
+decorButton.addEventListener("click", () => {
+  removeElements();
+  bagsButton.classList.remove("selected");
+  decorButton.classList.add("selected");
+  allButton.classList.remove("selected");
+  for (let i = 0; i < cards.length; i++) {
+    if (cards[i].type == 1)
+      items.prepend(
+        createElement(template, cards[i].img, cards[i].title, cards[i].price)
+      );
+  }
+});
+
+allButton.addEventListener("click", () => {
+  removeElements();
+  bagsButton.classList.remove("selected");
+  decorButton.classList.remove("selected");
+  allButton.classList.add("selected");
+  for (let i = 0; i < cards.length; i++) {
+    items.prepend(
+      createElement(template, cards[i].img, cards[i].title, cards[i].price)
+    );
+  }
+});
