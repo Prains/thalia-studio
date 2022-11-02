aca5483a40c8455559c3044c22df9703bc55a031
prains <sleskov2013@yandex.ru>
2022-11-03 01:09:54 +0300

    gallery for catalog

--- js/catalog-card.js
+++ js/catalog-card.js
@@ -1,13 +1,30 @@
 $(document).ready(() => {
   let cardTitle = $(".novelties__content__text-wrapper__title");
-  let cardImg = $('.novelties__image');
-  let cardDesc = $('.novelties__content__text-wrapper__text');
+  let cardImg = $(".novelties__image");
+  let cardDesc = $(".novelties__content__text-wrapper__text");
+  let galImg = $(".novelties__cards");
+
+  for (let i = 0; i < galImg.length; i++) {
+    $(galImg).hide();
+  }
 
   for (let i = 0; i < cards.length; i++) {
     if (window.location.href.indexOf(cards[i].code) != -1) {
-        $(cardTitle).text(cards[i].title);
-        $(cardImg).attr('src', cards[i].img);
-        $(cardDesc).text(cards[i].desc);
+      $(cardTitle).text(cards[i].title);
+      $(cardImg).attr("src", cards[i].img);
+      $(cardDesc).text(cards[i].desc);
+      for (let v = 0; v < cards[i].imges.length; v++) {
+        $(galImg[v]).attr("src", cards[i].imges[v]).show();
       }
+    }
+  }
+  for (let i = 0; i < galImg.length; i++) {
+    $(galImg[i]).click(() => {
+      $(".novelties__cards").removeClass("selected-card");
+      $(galImg[i]).addClass("selected-card");
+      $(cardImg).fadeOut(400, () => {
+        $(cardImg).attr("src", $(galImg[i]).attr("src")).fadeIn();
+      });
+    });
   }
 });
